96d0f7e5699a81ed44dc0da6e4922d2d79a8d0a7
Miguel Alcaino <miguel.alcaino@ogb.cl>
2018-04-19 01:53:48 +0100

    first commit

---
+++ src/Interfaces/Entity/TransactionRecordInterface.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: malcaino
+ * Date: 19/04/18
+ * Time: 01:38
+ */
+
+namespace MiguelAlcaino\PaymentGateway\Interfaces\Entity;
+
+
+interface TransactionRecordInterface
+{
+
+}
